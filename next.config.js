53f3b8fe0394c123ea769736e7a371071bde14ab
takumibv <everybody.say.hey.yo@gmail.com>
2021-11-07 15:40:25 +0900

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  basePath: process.env.GITHUB_ACTIONS ? "/color-cert-names-quiz" : "",
+  trailingSlash: true,
 }
